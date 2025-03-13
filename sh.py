9f3a558bbeac4ebc1ce262858ece9fe47dc8322e
Kirill Muravev <48129887+studokim@users.noreply.github.com>
2025-03-13 11:05:57 +0300

    Satisfy mypy and code formatter

--- sh.py
+++ sh.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 
-class Bash:
+from typing import Optional
+class BashFormatted:
     """
     Call Bash commands easily
     This class wraps subprocess.run()
@@ -10,17 +11,47 @@ class Bash:
     or      "echo hello" >> Bash()
     or      "ssh user@host" >> Bash(interactive=True)
     """
-    def __init__(self, interactive: bool=False):
+  
+    def __init__(self, interactive: bool = False) -> None:
         self.__i = interactive
-    def __call__(self, command: str) -> str:
-        if self.__i: return Bash.interactive(command)
-        else:        return Bash.execute(command)
-    def __rrshift__(self, command: str) -> str:
+ 
+    def __call__(self, command: str) -> Optional[str]:
+        if self.__i:
+            return Bash.interactive(command)
+        else:
+            return Bash.execute(command)
+
+    def __rrshift__(self, command: str) -> Optional[str]:
         return self.__call__(command)
+
+    @staticmethod
     def interactive(command: str) -> None:
         from subprocess import run
-        return run(["bash", "-c", command], check=True)
+   
+        run(["bash", "-c", command], check=True)
+
+    @staticmethod
     def execute(command: str) -> str:
+        from subprocess import run
+      
+        p = run(["bash", "-c", command], capture_output=True, text=True)
+        if p.returncode != 0:
+            raise Exception((p.stderr or p.stdout or f"Errno {p.returncode}").strip())
+        return p.stdout.strip()
+
+
+class Bash:
+    def __init__(self, interactive=False):
+        self.__i = interactive
+    def __call__(self, command):
+        if self.__i: return Bash.interactive(command)
+        else:        return Bash.execute(command)
+    def __rrshift__(self, command):
+        return self.__call__(command)
+    def interactive(command):
+        from subprocess import run
+        run(["bash", "-c", command], check=True)
+    def execute(command):
         from subprocess import run
         p = run(["bash", "-c", command], capture_output=True, text=True)
         if p.returncode != 0: raise Exception((p.stderr or p.stdout or f"Errno {p.returncode}").strip())
@@ -28,7 +59,7 @@ class Bash:
 
 
 def main():
-    out = 'echo Hello world' >> Bash()
+    out = "echo Hello world" >> Bash()
     print(out)
 
 
